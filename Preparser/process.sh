80662819c8fd54bdcb0b498ecf08ef6a295dae59
Jeremy Anticouni <jeremy@anticouni.net>
2013-11-26 20:43:12 -0800

    Integrated post build script processing.
    Signed-off-by: Jeremy Anticouni <jeremy@anticouni.net>

---
+++ Preparser/process.sh
@@ -0,0 +1,6 @@
+#rm -rf blah
+#mkdir blah
+#cp Ignite_iOS_Engine/assets/IXAppConfigChild.json Ignite_iOS_Engine/assets/IXAppConfigChild2.json 
+
+
+php Preparser/process.php Ignite_iOS_Engine/assets/IXAppConfigChild.json
