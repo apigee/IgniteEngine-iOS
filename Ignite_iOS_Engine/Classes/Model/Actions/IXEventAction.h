a2861ea7e9e29bf7e8a3379ec6a6ab66c3f7ec98
Robert Walsh <rwalsh@apigee.com>
2014-01-25 12:55:54 -0800

    Added the Event action.

---
+++ Ignite_iOS_Engine/Classes/Model/Actions/IXEventAction.h
@@ -0,0 +1,13 @@
+//
+//  IXEventAction.h
+//  Ignite_iOS_Engine
+//
+//  Created by Robert Walsh on 1/25/14.
+//  Copyright (c) 2014 Ignite. All rights reserved.
+//
+
+#import "IXBaseAction.h"
+
+@interface IXEventAction : IXBaseAction
+
+@end
---
+++ Ignite_iOS_Engine/Classes/Model/Actions/IXEventAction.m
@@ -0,0 +1,37 @@
+//
+//  IXEventAction.m
+//  Ignite_iOS_Engine
+//
+//  Created by Robert Walsh on 1/25/14.
+//  Copyright (c) 2014 Ignite. All rights reserved.
+//
+
+#import "IXEventAction.h"
+
+#import "IXSandbox.h"
+
+#import "IXPropertyContainer.h"
+#import "IXActionContainer.h"
+
+#import "IXBaseObject.h"
+
+@implementation IXEventAction
+
+-(void)execute
+{
+    IXPropertyContainer* actionProperties = [self actionProperties];
+    
+    NSString* objectID = [actionProperties getStringPropertyValue:@"id" defaultValue:nil];
+    NSString* eventName = [actionProperties getStringPropertyValue:@"event_name" defaultValue:nil];
+    
+    if( objectID && eventName )
+    {
+        NSArray* objectsWithID = [[[self actionContainer] sandbox] getAllControlAndDataProvidersWithID:objectID];
+        for( IXBaseObject* baseObject in objectsWithID )
+        {
+            [[baseObject actionContainer] executeActionsForEventNamed:eventName];
+        }
+    }
+}
+
+@end
