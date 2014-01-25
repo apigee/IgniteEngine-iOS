c0593a0a7587dcc50efa159d190ca3340783c6ba
Robert Walsh <rwalsh@apigee.com>
2014-01-24 17:03:23 -0800

    Added Eval Shortcode

---
+++ Ignite_iOS_Engine/Classes/Model/Properties/ShortCodes/IXEvalShortCode.h
@@ -0,0 +1,13 @@
+//
+//  IXEvalShortCode.h
+//  Ignite_iOS_Engine
+//
+//  Created by Robert Walsh on 1/24/14.
+//  Copyright (c) 2014 Ignite. All rights reserved.
+//
+
+#import "IXBaseShortCode.h"
+
+@interface IXEvalShortCode : IXBaseShortCode
+
+@end
---
+++ Ignite_iOS_Engine/Classes/Model/Properties/ShortCodes/IXEvalShortCode.m
@@ -0,0 +1,27 @@
+//
+//  IXEvalShortCode.m
+//  Ignite_iOS_Engine
+//
+//  Created by Robert Walsh on 1/24/14.
+//  Copyright (c) 2014 Ignite. All rights reserved.
+//
+
+#import "IXEvalShortCode.h"
+
+#import "IXAppManager.h"
+#import "IXProperty.h"
+
+@implementation IXEvalShortCode
+
+-(NSString*)evaluate:(IXSandbox*)sandbox
+{
+    NSString* stringToEval = [self methodName];
+    if( !stringToEval )
+    {
+        IXProperty* parameterProperty = (IXProperty*)[[self parameters] firstObject];
+        stringToEval = [parameterProperty getPropertyValue:sandbox];
+    }
+    return [[IXAppManager sharedAppManager] evaluateJavascript:stringToEval];
+}
+
+@end
