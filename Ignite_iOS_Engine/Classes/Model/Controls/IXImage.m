dc5dd62eef7f8064e4d5ea0059f5a8f3a31021f5
Robert Walsh <rwalsh@apigee.com>
2014-02-11 18:07:55 -0800

    Added "animated_images", "animation_duration", and "auto_animate" properties to the Image control. Added "start_animation" and "stop_animation" functions to the Image control.

--- Ignite_iOS_Engine/Classes/Model/Controls/IXImage.m
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXImage.m
@@ -13,6 +13,13 @@
 // IXImage Properties
 static NSString* const kIXImagesDefault = @"images.default";
 static NSString* const kIXImagesTouch = @"images.touch";
+static NSString* const kIXAnimatedImages = @"animated_images";
+static NSString* const kIXAnimationDuration = @"animated_images";
+static NSString* const kIXAutoAnimate = @"auto_animate";
+
+// IXImage Functions
+static NSString* const kIXStartAnimation = @"start_animation";
+static NSString* const kIXStopAnimation = @"stop_animation";
 
 @interface IXImage ()
 
@@ -21,6 +28,7 @@ static NSString* const kIXImagesTouch = @"images.touch";
 @property (nonatomic,strong) UIImage* defaultImage;
 @property (nonatomic,strong) NSString* touchedImagePath;
 @property (nonatomic,strong) UIImage* touchedImage;
+@property (nonatomic,strong) NSArray* animatedImages;
 
 @end
 
@@ -48,19 +56,47 @@ static NSString* const kIXImagesTouch = @"images.touch";
 {
     [super applySettings];
 
-    __weak IXImage* weakSelf = self;
-    [[self propertyContainer] getImageProperty:kIXImagesDefault
-                                  successBlock:^(UIImage *image) {
-                                      [weakSelf setDefaultImage:image];
-                                      [[weakSelf imageView] setImage:image];
-                                  } failBlock:^(NSError *error) {
-                                  }];
-    
-    [[self propertyContainer] getImageProperty:kIXImagesTouch
-                                  successBlock:^(UIImage *image) {
-                                      [weakSelf setTouchedImage:image];
-                                  } failBlock:^(NSError *error) {
-                                  }];    
+    if( [[self propertyContainer] propertyExistsForPropertyNamed:kIXAnimatedImages] )
+    {
+        NSArray* imagesPaths = [[self propertyContainer] getCommaSeperatedArrayListValue:kIXAnimatedImages defaultValue:nil];
+        if( [imagesPaths count] )
+        {
+            NSMutableArray* imagesArray = [NSMutableArray arrayWithCapacity:[imagesPaths count]];
+            for( NSString* imagePath in imagesPaths )
+            {
+                UIImage* image = [UIImage imageNamed:imagePath];
+                if( image )
+                {
+                    [imagesArray addObject:image];
+                }
+            }
+            [[self imageView] setAnimationImages:imagesArray];
+            [[self imageView] setAnimationDuration:[[self propertyContainer] getFloatPropertyValue:kIXAnimationDuration defaultValue:0.2f]];
+            
+            BOOL autoAnimate = [[self propertyContainer] getBoolPropertyValue:kIXAutoAnimate defaultValue:YES];
+            if( autoAnimate )
+            {
+                [[self imageView] startAnimating];
+            }
+        }
+    }
+    else
+    {
+        __weak IXImage* weakSelf = self;
+        [[self propertyContainer] getImageProperty:kIXImagesDefault
+                                      successBlock:^(UIImage *image) {
+                                          [weakSelf setDefaultImage:image];
+                                          [[weakSelf imageView] setImage:image];
+                                      } failBlock:^(NSError *error) {
+                                      }];
+        
+        [[self propertyContainer] getImageProperty:kIXImagesTouch
+                                      successBlock:^(UIImage *image) {
+                                          [weakSelf setTouchedImage:image];
+                                      } failBlock:^(NSError *error) {
+                                      }];
+
+    }
 }
 
 -(void)controlViewTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -82,6 +118,22 @@ static NSString* const kIXImagesTouch = @"images.touch";
 {
     [super controlViewTouchesEnded:touches withEvent:event];
     [[self imageView] setImage:[self defaultImage]];
+}
+
+-(void)applyFunction:(NSString *)functionName withParameters:(IXPropertyContainer *)parameterContainer
+{
+    if( [functionName isEqualToString:kIXStartAnimation] )
+    {
+        [[self imageView] startAnimating];
+    }
+    else if( [functionName isEqualToString:kIXStopAnimation] )
+    {
+        [[self imageView] stopAnimating];
+    }
+    else
+    {
+        [super applyFunction:functionName withParameters:parameterContainer];
+    }
 }
 
 @end
