0b51c6b27d4b9232ba44240d6092ccaa3563fe4b
Robert Walsh <rwalsh@apigee.com>
2014-02-12 17:35:53 -0800

    Updated Knob control to use a different 3rd party library.

--- Ignite_iOS_Engine/Classes/Model/Controls/IXKnob.m
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXKnob.m
@@ -8,8 +8,7 @@
 
 #import "IXKnob.h"
 
-#import "RWKnobControl.h"
-#import "RWKnobRenderer.h"
+#import "MHRotaryKnob.h"
 
 // Knob Properties
 static NSString* const kIXInitialValue = @"initial_value";
@@ -19,6 +18,9 @@ static NSString* const kIXMinimumValue = @"minimum_value";
 static NSString* const kIXMaximumValue = @"maximum_value";
 static NSString* const kIXTrackColor = @"track_color";
 static NSString* const kIXPointerColor = @"pointer_color";
+static NSString* const kIXImagesForeground = @"images.foreground";
+static NSString* const kIXImagesBackground = @"images.background";
+static NSString* const kIXImagesPointer = @"images.pointer";
 static NSString* const kIXTrackVisible = @"track_visible";
 static NSString* const kIXPointerVisible = @"pointer_visible";
 static NSString* const kIXStartAngle = @"start_angle";
@@ -37,7 +39,7 @@ static NSString* const kIXAnimated = @"animated"; // Parameter of the "update_kn
 @interface IXKnob ()
 
 @property (nonatomic,assign,getter = isFirstLoad) BOOL firstLoad;
-@property (nonatomic,strong) RWKnobControl* knobControl;
+@property (nonatomic,strong) MHRotaryKnob* knobControl;
 
 @end
 
@@ -48,9 +50,8 @@ static NSString* const kIXAnimated = @"animated"; // Parameter of the "update_kn
     [super buildView];
     
     _firstLoad = YES;
-
-    _knobControl = [[RWKnobControl alloc] initWithFrame:CGRectZero];
     
+    _knobControl = [[MHRotaryKnob alloc] initWithFrame:CGRectZero];
     [_knobControl addTarget:self action:@selector(knobValueChanged:) forControlEvents:UIControlEventValueChanged];
 
     [[self contentView] addSubview:_knobControl];
@@ -63,43 +64,49 @@ static NSString* const kIXAnimated = @"animated"; // Parameter of the "update_kn
 
 -(void)layoutControlContentsInRect:(CGRect)rect
 {
-    [[self knobControl] setFrame:rect];
-    [[[self knobControl] knobRenderer] updateWithBounds:rect];
+    CGPoint center = CGPointMake(rect.origin.x + (rect.size.width / 2), rect.origin.y + (rect.size.height / 2));
+    if(!CGRectEqualToRect([[self knobControl] frame], rect) || !CGPointEqualToPoint([[self knobControl] center], center) )
+    {
+        [[self knobControl] setFrame:rect];
+        
+        _knobControl.knobImageCenter = center;
+        [self updateKnobValueWithValue:[[self knobControl] value] animated:NO];
+    }
 }
 
 -(void)applySettings
 {
     [super applySettings];
     
-    [[self knobControl] setLineWidth:[[self propertyContainer] getFloatPropertyValue:kIXLineWidth defaultValue:2.0f]];
-    [[self knobControl] setPointerLength:[[self propertyContainer] getFloatPropertyValue:kIXPointerLength defaultValue:10.0f]];
     [[self knobControl] setMinimumValue:[[self propertyContainer] getFloatPropertyValue:kIXMinimumValue defaultValue:0.0f]];
     [[self knobControl] setMaximumValue:[[self propertyContainer] getFloatPropertyValue:kIXMaximumValue defaultValue:1.0f]];
     
-    CGFloat startAngle = [[self propertyContainer] getFloatPropertyValue:kIXStartAngle defaultValue:-M_PI * 11 / 8.0];
-    CGFloat endAngle = [[self propertyContainer] getFloatPropertyValue:kIXEndAngle defaultValue:M_PI * 3 / 8.0];
-    [[self knobControl] setStartAngle:startAngle];
-    [[self knobControl] setEndAngle:endAngle];
-    
-    UIColor* trackColor = [[self propertyContainer] getColorPropertyValue:kIXTrackColor defaultValue:[UIColor blueColor]];
-    UIColor* pointerColor = [[self propertyContainer] getColorPropertyValue:kIXPointerColor defaultValue:[UIColor redColor]];
-    [[[[self knobControl] knobRenderer] trackLayer] setStrokeColor:[trackColor CGColor]];
-    [[[[self knobControl] knobRenderer] pointerLayer] setStrokeColor:[pointerColor CGColor]];
-    
-    BOOL trackVisible = [[self propertyContainer] getBoolPropertyValue:kIXTrackVisible defaultValue:NO];
-    BOOL pointerVisible = [[self propertyContainer] getBoolPropertyValue:kIXPointerVisible defaultValue:YES];
-    [[[[self knobControl] knobRenderer] trackLayer] setHidden:!trackVisible];
-    [[[[self knobControl] knobRenderer] pointerLayer] setHidden:!pointerVisible];
+    [[self propertyContainer] getImageProperty:kIXImagesPointer
+                                  successBlock:^(UIImage *image) {
+                                      if(image)
+                                      {
+                                          [[self knobControl] setKnobImage:image forState:UIControlStateNormal];
+                                      }
+                                  } failBlock:^(NSError *error) {
+                                  }];
+    [[self propertyContainer] getImageProperty:kIXImagesBackground
+                                  successBlock:^(UIImage *image) {
+                                      if(image)
+                                      {
+                                          [[self knobControl] setBackgroundImage:image];
+                                      }
+                                  } failBlock:^(NSError *error) {
+                                  }];
     
     if( [self isFirstLoad] )
     {
         [self setFirstLoad:NO];
         CGFloat initialValue = [[self propertyContainer] getFloatPropertyValue:kIXInitialValue defaultValue:0];
-        [self updateKnobValueWithValue:initialValue animated:YES];
+        [self updateKnobValueWithValue:initialValue animated:NO];
     }
 }
 
--(void)knobValueChanged:(RWKnobControl*)knobControl
+-(void)knobValueChanged:(MHRotaryKnob*)knobControl
 {
     [[self actionContainer] executeActionsForEventNamed:IXValueChanged];
 }
---
+++ Ignite_iOS_Engine/Classes/ThirdParty/MHRotaryKnob/MHRotaryKnob.h
@@ -0,0 +1,136 @@
+/*
+ * UIControl subclass that acts like a rotary knob.
+ *
+ * Copyright (c) 2010-2012 Matthijs Hollemans
+ *
+ * With contributions from Tim Kemp (slider-style tracking).
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import <UIKit/UIKit.h>
+
+/*
+ * Possible values for the rotary knob's interactionStyle property.
+ */
+typedef enum
+{
+	MHRotaryKnobInteractionStyleRotating,
+	MHRotaryKnobInteractionStyleSliderHorizontal,  // left -, right +
+	MHRotaryKnobInteractionStyleSliderVertical     // up +, down -
+}
+MHRotaryKnobInteractionStyle;
+
+/*
+ * A rotary knob control.
+ *
+ * Operation of this control is similar to a UISlider. You can set a minimum,
+ * maximum, and current value. When the knob is turned the control sends out
+ * a UIControlEventValueChanged notification to its target-action.
+ *
+ * The interactionStyle property determines the way the control is operated.
+ * It can be configured to act like a knob that must be turned, or to act like
+ * a horizontal or vertical slider.
+ *
+ * The control uses two images, one for the background and one for the knob. 
+ * The background image is optional but you must set at least the knob image
+ * before you can use the control.
+ *
+ * When double-tapped, the control resets to its default value, typically the
+ * the center or minimum position. This feature can be disabled with the
+ * resetsToDefault property.
+ *
+ * Because users will want to see what happens under their fingers, you are 
+ * advised not to make the knob smaller than 120x120 points. Because of this, 
+ * rotary knob controls probably work best on an iPad.
+ *
+ * This class needs the QuartzCore framework.
+ */
+@interface MHRotaryKnob : UIControl
+
+/* How the user interacts with the control. */
+@property (nonatomic, assign) MHRotaryKnobInteractionStyle interactionStyle;
+
+/* The image that is drawn behind the knob. May be nil. */
+@property (nonatomic, strong) UIImage* backgroundImage;
+
+/*
+ * The image that is drawn on top of the knob. May be nil. This is useful
+ * for partially transparent overlays to make shadow or highlight effects.
+ */
+@property (nonatomic, strong) UIImage* foregroundImage;
+
+/* The image currently being used to draw the knob. */
+@property (nonatomic, strong, readonly) UIImage* currentKnobImage;
+
+/* For positioning the knob image. */
+@property (nonatomic, assign) CGPoint knobImageCenter;
+
+/* The maximum value of the control. Default is 1.0f. */
+@property (nonatomic, assign) float maximumValue;
+
+/* The minimum value of the control. Default is 0.0f. */
+@property (nonatomic, assign) float minimumValue;
+
+/* The control's current value. Default is 0.5f (center position). */
+@property (nonatomic, assign) float value;
+
+/* The control's default value. Default is 0.5f (center position). */
+@property (nonatomic, assign) float defaultValue;
+
+/*
+ * Whether the control resets to the default value on a double tap.
+ * Default is YES.
+ */
+@property (nonatomic, assign) BOOL resetsToDefault;
+
+/*
+ * Whether changes in the knob's value generate continuous update events. 
+ * If NO, the control only sends an action event when the user releases the 
+ * knob. The default is YES.
+ */
+@property (nonatomic, assign) BOOL continuous;
+
+/*
+ * How many points of movement result in a one degree rotation in the knob's
+ * position. Only used in the horizontal/vertical slider modes. Default is 1.
+ */
+@property (nonatomic, assign) float scalingFactor;
+
+/*
+ * Sets the controls’s current value, allowing you to animate the change
+ * visually.
+ */
+- (void)setValue:(float)value animated:(BOOL)animated;
+
+/*
+ * Assigns a knob image to the specified control states.
+ * 
+ * This image should have its position indicator at the top. The knob image is
+ * rotated when the control's value changes, so it's best to make it perfectly
+ * round.
+ */
+- (void)setKnobImage:(UIImage *)image forState:(UIControlState)state;
+
+/*
+ * Returns the thumb image associated with the specified control state.
+ */
+- (UIImage *)knobImageForState:(UIControlState)state;
+
+@end
