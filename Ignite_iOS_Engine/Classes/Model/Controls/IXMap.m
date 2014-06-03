17d28a433e20f6d8408207ed7de87eda7a44a26d
Robert Walsh <rjwalsh1985@gmail.com>
2014-06-03 14:38:10 -0700

    Updated the Map control.

--- Ignite_iOS_Engine/Classes/Model/Controls/IXMap.m
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXMap.m
@@ -27,19 +27,22 @@
 
 @import MapKit;
 
+#import "IXBaseDataProvider.h"
+
 #import "SVPulsingAnnotationView.h"
 #import "SVAnnotation.h"
 
-@interface IXMap ()
+static NSString* const kIXDataProviderID = @"dataprovider_id";
+static NSString* const kIXShowsUserLocation = @"shows_user_location";
 
-@property (nonatomic,strong) MKMapView* mapView;
-@property (nonatomic,strong) NSArray *mIXtemList;
-@property (nonatomic,assign) MKCoordinateRegion boundingRegion;
-@property (nonatomic,readwrite) CLLocationCoordinate2D coordinate;
-@property (nonatomic,copy) NSString *title;
-@property (nonatomic,copy) NSString *subtitle;
+static NSString* const kIXAnnotationImage = @"annotation.image";
+static NSString* const kIXAnnotationTitle = @"annotation.title";
+static NSString* const kIXAnnotationSubTitle = @"annotation.subtitle";
+static NSString* const kIXAnnotationLatitude = @"annotation.latitude";
+static NSString* const kIXAnnotationLongitude = @"annotation.longitude";
 
-@end
+static NSString* const kIXTouch = @"touch";
+static NSString* const kIXTouchUp = @"touch_up";
 
 @implementation SVAnnotation
 
@@ -51,56 +54,249 @@
 
 @end
 
+@interface IXMapAnnotation : NSObject <MKAnnotation>
+
+- (instancetype)initWithCoordinate:(CLLocationCoordinate2D)coordinate
+                             title:(NSString*)title
+                          subtitle:(NSString*)subTitle
+                  dataRowIndexPath:(NSIndexPath*)dataRowIndexPath;
+
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+@property (nonatomic, strong) NSIndexPath *dataRowIndexPath;
+
+@end
+
+@implementation IXMapAnnotation
+
+- (instancetype)initWithCoordinate:(CLLocationCoordinate2D)coordinate
+                             title:(NSString*)title
+                          subtitle:(NSString*)subTitle
+                  dataRowIndexPath:(NSIndexPath*)dataRowIndexPath
+{
+    self = [super init];
+    if( self )
+    {
+        _coordinate = coordinate;
+        _title = [title copy];
+        _subtitle = [subTitle copy];
+        _dataRowIndexPath = dataRowIndexPath;
+    }
+    return self;
+}
+
+@end
+
+@interface IXMap () <MKMapViewDelegate>
+
+@property (nonatomic,strong) MKMapView* mapView;
+
+@property (nonatomic, strong) NSString* dataProviderID;
+@property (nonatomic,weak) IXBaseDataProvider* dataProvider;
+
+@property (nonatomic,strong) NSMutableArray* annotations;
+
+@property (nonatomic,copy) NSString* annotationImagePath;
+@property (nonatomic,copy) NSString* annotationTitlePath;
+@property (nonatomic,copy) NSString* annotationSubTitlePath;
+@property (nonatomic,copy) NSString* annotationLatitudePath;
+@property (nonatomic,copy) NSString* annotationLongitudePath;
+
+@end
+
 @implementation IXMap
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:IXBaseDataProviderDidUpdateNotification
+                                                  object:[self dataProvider]];
+    [_mapView setDelegate:nil];
+}
 
 -(void)buildView
 {
     [super buildView];
-    _mapView = [[MKMapView alloc] initWithFrame: CGRectZero];
+    
+    _annotations = [[NSMutableArray alloc] init];
+    
+    _mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 100.0f, 100.0f)];
+    [_mapView setDelegate:self];
+    
+    [[self contentView] addSubview:_mapView];
+}
+
+-(CGSize)preferredSizeForSuggestedSize:(CGSize)size
+{
+    return [[self mapView] sizeThatFits:size];
+}
+
+-(void)layoutControlContentsInRect:(CGRect)rect
+{
+    [[self mapView] setFrame:rect];
 }
 
 -(void)applySettings
 {
     [super applySettings];
-
-    _mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0, 0, [[self propertyContainer] getFloatPropertyValue:@"width" defaultValue:0],
-                                                           [[self propertyContainer] getFloatPropertyValue:@"height" defaultValue:0])];
     
-    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake([[self propertyContainer] getFloatPropertyValue:@"placemark.latitude" defaultValue:37.331789],
-                                                                   [[self propertyContainer] getFloatPropertyValue:@"placemark.longitude" defaultValue:-122.029620]);
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:IXBaseDataProviderDidUpdateNotification
+                                                  object:[self dataProvider]];
     
-    MKCoordinateRegion region = MKCoordinateRegionMake(coordinate, MKCoordinateSpanMake(0.1, 0.1));
-    [self.mapView setRegion:region animated:NO];
+    [self setDataProviderID:[[self propertyContainer] getStringPropertyValue:kIXDataProviderID defaultValue:nil]];
+    [self setDataProvider:[[self sandbox] getDataProviderWithID:[self dataProviderID]]];
     
-    SVAnnotation *annotation = [[SVAnnotation alloc] initWithCoordinate:coordinate];
-    annotation.title = [[self propertyContainer] getStringPropertyValue:@"placemark.title" defaultValue:@"title"];
-    annotation.subtitle = [[self propertyContainer] getStringPropertyValue:@"placemark.subtitle" defaultValue:@"subtitle"];
-    [self.mapView addAnnotation:annotation];
-    [[self contentView] addSubview:_mapView];
-    
-}
-
-
-- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
-    if([annotation isKindOfClass:[SVAnnotation class]]) {
-        static NSString *identifier = @"currentLocation";
-		SVPulsingAnnotationView *pulsingView = (SVPulsingAnnotationView *)[self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
-		
-		if(pulsingView == nil) {
-			pulsingView = [[SVPulsingAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
-            pulsingView.annotationColor = [UIColor colorWithRed:0.678431 green:0 blue:0 alpha:1];
-            pulsingView.canShowCallout = YES;
-        }
-		
-		return pulsingView;
+    if( [self dataProvider] )
+    {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(dataProviderNotification:)
+                                                     name:IXBaseDataProviderDidUpdateNotification
+                                                   object:[self dataProvider]];
     }
     
-    return nil;
+    [[self mapView] setShowsUserLocation:[[self propertyContainer] getBoolPropertyValue:kIXShowsUserLocation defaultValue:NO]];
+    
+    [self reloadMapAnnotations];
 }
 
+-(void)dataProviderNotification:(NSNotification*)notification
+{
+    [self reloadMapAnnotations];
+}
 
+-(void)reloadMapAnnotations
+{
+    [[self mapView] removeAnnotations:[self annotations]];
+    [[self annotations] removeAllObjects];
+    
+    // Save off the Map controls original index path and dataprovider for the row data so we can reset it after we set up the annotations.
+    NSIndexPath* currentSandboxIndexPath = [[self sandbox] indexPathForRowData];
+    IXBaseDataProvider* currentSandboxDataProvider = [[self sandbox] dataProviderForRowData];
+    
+    [[self sandbox] setDataProviderForRowData:[self dataProvider]];
+    for( int i = 0; i < [[self dataProvider] rowCount]; i++ )
+    {
+        [[self sandbox] setIndexPathForRowData:[NSIndexPath indexPathForRow:i inSection:0]];
+        
+        CGFloat annotationLatitude = [[self propertyContainer] getFloatPropertyValue:kIXAnnotationLatitude defaultValue:0.0f];
+        CGFloat annotationLongitude = [[self propertyContainer] getFloatPropertyValue:kIXAnnotationLongitude defaultValue:0.0f];
+        
+        IXMapAnnotation *annotation = [[IXMapAnnotation alloc] initWithCoordinate:CLLocationCoordinate2DMake(annotationLatitude, annotationLongitude)
+                                                                            title:[[self propertyContainer] getStringPropertyValue:kIXAnnotationTitle
+                                                                                                                      defaultValue:nil]
+                                                                         subtitle:[[self propertyContainer] getStringPropertyValue:kIXAnnotationSubTitle
+                                                                                                                      defaultValue:nil]
+                                                                 dataRowIndexPath:[[self sandbox] indexPathForRowData]];
+        if( annotation )
+        {
+            [[self annotations] addObject:annotation];
+        }
+    }
+    
+    // Reset the Map controls sandbox values.
+    [[self sandbox] setIndexPathForRowData:currentSandboxIndexPath];
+    [[self sandbox] setDataProviderForRowData:currentSandboxDataProvider];
+    
+    [[self mapView] showAnnotations:[self annotations] animated:YES];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    MKAnnotationView* annotationView = nil;
+    if( [annotation isKindOfClass:[IXMapAnnotation class]] )
+    {
+        // Save off the Map controls original index path and dataprovider for the row data so we can reset it after we set up the annotations views.
+        NSIndexPath* currentSandboxIndexPath = [[self sandbox] indexPathForRowData];
+        IXBaseDataProvider* currentSandboxDataProvider = [[self sandbox] dataProviderForRowData];
+
+        IXMapAnnotation* mapAnnotation = (IXMapAnnotation*)annotation;
+        NSIndexPath* indexPathForAnnotation = [mapAnnotation dataRowIndexPath];
+        
+        [[self sandbox] setIndexPathForRowData:indexPathForAnnotation];
+        [[self sandbox] setDataProviderForRowData:[self dataProvider]];
+        
+        NSString* imageLocation = [[self propertyContainer] getStringPropertyValue:kIXAnnotationImage defaultValue:nil];
+        if( [imageLocation length] > 0 )
+        {
+            static NSString *kIXMapImageAnnotationIdentifier = @"kIXMapImageAnnotationIdentifier";
+            annotationView = [[self mapView] dequeueReusableAnnotationViewWithIdentifier:kIXMapImageAnnotationIdentifier];
+            
+            if( annotationView == nil )
+            {
+                annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:kIXMapImageAnnotationIdentifier];
+                [annotationView setCanShowCallout:YES];
+            }
+            
+            if( annotationView )
+            {
+                [[self propertyContainer] getImageProperty:kIXAnnotationImage
+                                              successBlock:^(UIImage *image) {
+                                                  [annotationView setImage:image];
+                                              } failBlock:^(NSError *error) {
+                                                  [annotationView setImage:nil];
+                                              }];
+            }
+        }
+        else
+        {
+            static NSString *kIXMapPinAnnotationIdentifier = @"kIXMapPinAnnotationIdentifier";
+            annotationView = (MKPinAnnotationView*)[[self mapView] dequeueReusableAnnotationViewWithIdentifier:kIXMapPinAnnotationIdentifier];
+            
+            if( annotationView == nil )
+            {
+                annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:kIXMapPinAnnotationIdentifier];
+                [annotationView setCanShowCallout:YES];
+            }
+        }
+        
+        if( [self actionContainer] != nil )
+        {
+            [annotationView setRightCalloutAccessoryView:[UIButton buttonWithType:UIButtonTypeDetailDisclosure]];
+        }
+        else
+        {
+            [annotationView setRightCalloutAccessoryView:nil];
+        }
+        
+        // Reset the Map controls sandbox values.
+        [[self sandbox] setIndexPathForRowData:currentSandboxIndexPath];
+        [[self sandbox] setDataProviderForRowData:currentSandboxDataProvider];
+    }
+    return annotationView;
+}
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    if( [control isEqual:[view rightCalloutAccessoryView]] && [[view annotation] isKindOfClass:[IXMapAnnotation class]] )
+    {
+        // Save off the Map controls original index path and dataprovider for the row data so we can reset it after we fire the actions on the annotations.
+        NSIndexPath* currentSandboxIndexPath = [[self sandbox] indexPathForRowData];
+        IXBaseDataProvider* currentSandboxDataProvider = [[self sandbox] dataProviderForRowData];
+
+        IXMapAnnotation* mapAnnotation = (IXMapAnnotation*)[view annotation];
+        NSIndexPath* indexPathForAnnotation = [mapAnnotation dataRowIndexPath];
+
+        [[self sandbox] setIndexPathForRowData:indexPathForAnnotation];
+        [[self sandbox] setDataProviderForRowData:[self dataProvider]];
+        
+        [[self actionContainer] executeActionsForEventNamed:kIXTouch];
+        [[self actionContainer] executeActionsForEventNamed:kIXTouchUp];
+
+        // Reset the Map controls sandbox values.
+        [[self sandbox] setIndexPathForRowData:currentSandboxIndexPath];
+        [[self sandbox] setDataProviderForRowData:currentSandboxDataProvider];
+    }
+}
+
+-(void)processEndTouch:(BOOL)fireTouchActions
+{
+    // Map doesnt need to fire any touch actions.
+}
+-(void)processBeginTouch:(BOOL)fireTouchActions
+{
+    // Map doesnt need to fire any touch actions.
+}
 
 @end
-
-
-
