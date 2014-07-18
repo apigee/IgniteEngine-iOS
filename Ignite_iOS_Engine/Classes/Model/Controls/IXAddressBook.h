0b7f803f8f913ee886914b0625aaf6cf4f12004f
Robert Walsh <rjwalsh1985@gmail.com>
2014-07-18 15:58:53 -0700

    Added the Event and AddressBook controls.

---
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXAddressBook.h
@@ -0,0 +1,13 @@
+//
+//  IXAddressBook.h
+//  Ignite_iOS_Engine
+//
+//  Created by Robert Walsh on 7/18/14.
+//  Copyright (c) 2014 Ignite. All rights reserved.
+//
+
+#import "IXBaseControl.h"
+
+@interface IXAddressBook : IXBaseControl
+
+@end
