33767dfab45838ab5cc6d111c7840626e5483e0b
Jeremy Anticouni <jeremy@anticouni.net>
2014-08-19 18:32:26 -0700

    Added input.format.credit_card

--- Ignite_iOS_Engine/Classes/Model/Controls/IXTextInput.m
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXTextInput.m
@@ -65,6 +65,7 @@ static NSString* const kIXKeyboardPadding = @"keyboard.padding";
 static NSString* const kIXKeyboardReturnKey = @"keyboard.return_key";
 
 IX_STATIC_CONST_STRING kIXInputFormatCurrency = @"input.format.currency";
+IX_STATIC_CONST_STRING kIXInputFormatCreditCard = @"input.format.credit_card";
 static NSString* const kIXInputRegexAllowed = @"input.regex.allowed";
 static NSString* const kIXInputRegexDisAllowed = @"input.regex.disallowed";
 static NSString* const kIXInputMax = @"input.max";
@@ -123,6 +124,11 @@ static NSString* const kIXNewLineString = @"\n";
 @property (nonatomic,assign) CGSize layoutContentSizeAtStartOfEditing;
 
 @property (nonatomic,assign) BOOL inputFormatCurrency;
+@property (nonatomic,assign) BOOL inputFormatCreditCard;
+
+@property (nonatomic,assign) NSString *previousTextFieldContent;
+@property (nonatomic,assign) UITextRange *previousSelection;
+
 @property (nonatomic,assign) NSInteger inputMaxAllowedCharacters;
 @property (nonatomic,strong) NSString* inputTransform;
 @property (nonatomic,strong) NSString* inputAllowedRegexString;
@@ -359,6 +365,8 @@ static NSString* const kIXNewLineString = @"\n";
     
     [self setInputFormatCurrency:[[self propertyContainer] getBoolPropertyValue:kIXInputFormatCurrency defaultValue:NO]];
     
+    [self setInputFormatCreditCard:[[self propertyContainer] getBoolPropertyValue:kIXInputFormatCreditCard defaultValue:NO]];
+    
     [self setInputAllowedRegexString:[[self propertyContainer] getStringPropertyValue:kIXInputRegexAllowed defaultValue:nil]];
     if( [[self inputAllowedRegexString] length] > 1 )
     {
@@ -386,6 +394,8 @@ static NSString* const kIXNewLineString = @"\n";
     
     [self setKeyboardPadding:[[self propertyContainer] getFloatPropertyValue:kIXKeyboardPadding defaultValue:0.0f]];
     [self setLayoutToScroll:layoutToScroll];
+    
+    
 }
 
 -(void)rightViewTapDetected:(UITapGestureRecognizer*)tapRecognizer
@@ -651,6 +661,112 @@ static NSString* const kIXNewLineString = @"\n";
     }
 }
 
+// Credit Card Formatting
+// Version 1.2
+// Source and explanation: http://stackoverflow.com/a/19161529/1709587
+-(void)reformatAsCardNumber:(UITextField *)textField
+{
+    // In order to make the cursor end up positioned correctly, we need to
+    // explicitly reposition it after we inject spaces into the text.
+    // targetCursorPosition keeps track of where the cursor needs to end up as
+    // we modify the string, and at the end we set the cursor position to it.
+    NSUInteger targetCursorPosition =
+    [textField offsetFromPosition:textField.beginningOfDocument
+                       toPosition:textField.selectedTextRange.start];
+    
+    NSString *cardNumberWithoutSpaces =
+    [self removeNonDigits:textField.text
+andPreserveCursorPosition:&targetCursorPosition];
+    
+    if ([cardNumberWithoutSpaces length] > 19) {
+        // If the user is trying to enter more than 19 digits, we prevent
+        // their change, leaving the text field in  its previous state.
+        // While 16 digits is usual, credit card numbers have a hard
+        // maximum of 19 digits defined by ISO standard 7812-1 in section
+        // 3.8 and elsewhere. Applying this hard maximum here rather than
+        // a maximum of 16 ensures that users with unusual card numbers
+        // will still be able to enter their card number even if the
+        // resultant formatting is odd.
+        [textField setText:_previousTextFieldContent];
+        textField.selectedTextRange = _previousSelection;
+        return;
+    }
+    
+    NSString *cardNumberWithSpaces =
+    [self insertSpacesEveryFourDigitsIntoString:cardNumberWithoutSpaces
+                      andPreserveCursorPosition:&targetCursorPosition];
+    
+    textField.text = cardNumberWithSpaces;
+    UITextPosition *targetPosition =
+    [textField positionFromPosition:[textField beginningOfDocument]
+                             offset:targetCursorPosition];
+    
+    [textField setSelectedTextRange:
+     [textField textRangeFromPosition:targetPosition
+                           toPosition:targetPosition]
+     ];
+}
+
+/*
+ Removes non-digits from the string, decrementing `cursorPosition` as
+ appropriate so that, for instance, if we pass in `@"1111 1123 1111"`
+ and a cursor position of `8`, the cursor position will be changed to
+ `7` (keeping it between the '2' and the '3' after the spaces are removed).
+ */
+- (NSString *)removeNonDigits:(NSString *)string
+    andPreserveCursorPosition:(NSUInteger *)cursorPosition
+{
+    NSUInteger originalCursorPosition = *cursorPosition;
+    NSMutableString *digitsOnlyString = [NSMutableString new];
+    for (NSUInteger i=0; i<[string length]; i++) {
+        unichar characterToAdd = [string characterAtIndex:i];
+        if (isdigit(characterToAdd)) {
+            NSString *stringToAdd =
+            [NSString stringWithCharacters:&characterToAdd
+                                    length:1];
+            
+            [digitsOnlyString appendString:stringToAdd];
+        }
+        else {
+            if (i < originalCursorPosition) {
+                (*cursorPosition)--;
+            }
+        }
+    }
+    
+    return digitsOnlyString;
+}
+
+/*
+ Inserts spaces into the string to format it as a credit card number,
+ incrementing `cursorPosition` as appropriate so that, for instance, if we
+ pass in `@"111111231111"` and a cursor position of `7`, the cursor position
+ will be changed to `8` (keeping it between the '2' and the '3' after the
+ spaces are added).
+ */
+- (NSString *)insertSpacesEveryFourDigitsIntoString:(NSString *)string
+                          andPreserveCursorPosition:(NSUInteger *)cursorPosition
+{
+    NSMutableString *stringWithAddedSpaces = [NSMutableString new];
+    NSUInteger cursorPositionInSpacelessString = *cursorPosition;
+    for (NSUInteger i=0; i<[string length]; i++) {
+        if ((i>0) && ((i % 4) == 0)) {
+            [stringWithAddedSpaces appendString:@" "];
+            if (i < cursorPositionInSpacelessString) {
+                (*cursorPosition)++;
+            }
+        }
+        unichar characterToAdd = [string characterAtIndex:i];
+        NSString *stringToAdd =
+        [NSString stringWithCharacters:&characterToAdd length:1];
+        
+        [stringWithAddedSpaces appendString:stringToAdd];
+    }
+    
+    return stringWithAddedSpaces;
+}
+
+
 - (void)textInputDidEndEditing:(id<UITextInput>)textInput
 {
     if( [self adjustsScrollWithScreen] )
@@ -755,6 +871,14 @@ static NSString* const kIXNewLineString = @"\n";
 
 -(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
     
+    if( [self inputFormatCreditCard] )
+    {
+        _previousTextFieldContent = textField.text;
+        _previousSelection = textField.selectedTextRange;
+        
+        [self reformatAsCardNumber:_textField];
+    }
+    
     if( [self inputFormatCurrency] )
     {
         NSMutableCharacterSet *numberSet = [[NSCharacterSet decimalDigitCharacterSet] mutableCopy];
@@ -823,7 +947,9 @@ static NSString* const kIXNewLineString = @"\n";
         }
         return NO;
     }
+    
     return YES;
+    
 }
 
 
