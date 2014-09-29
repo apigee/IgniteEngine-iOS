578a0a653e7d69c0ecc77b97cf7feaa50675012d
Robert Walsh <rjwalsh1985@gmail.com>
2014-09-29 12:39:59 -0700

    Updated the TouchID control code.

--- Ignite_iOS_Engine/Classes/Model/Controls/IXTouchID.m
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXTouchID.m
@@ -57,45 +57,47 @@ static NSString* const kIXAuthenticationUnavailable = @"unavailable";
             [_context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
                       localizedReason:title
                                 reply:^(BOOL success, NSError *error) {
-                                    
-                                    if (success) {
-                                        /* why is there a delay between when the user authenticates successfully 
-                                         and the action execution?
-                                         
-                                         - Can take as long as 20s before an Alert/Modify occurs visually.
-                                         - Appears to be a UI-only issue -- meaning that the action fires, 
-                                         but the effect of the action is delayed visually.
-                                         - When it occurs, the UI is blocked and unresponsive
-                                         - Appears to only occur when the action affects the UI -- Log is immediate
-                                         
-                                         */
-                                        IX_LOG_VERBOSE(@"User authenticated successfully.");
-                                        [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationSuccess];
-                                        
-                                    } else {
-                                        
-                                        switch (error.code) {
-                                            case LAErrorAuthenticationFailed:
-                                                [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationFailed];
-                                                IX_LOG_VERBOSE(@"Authenticated failed.");
-                                                break;
-                                                
-                                            case LAErrorUserCancel:
-                                                [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationCancelled];
-                                                IX_LOG_VERBOSE(@"User cancelled authentication.");
-                                                break;
-                                                
-                                            case LAErrorUserFallback:
-                                                [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationPassword];
-                                                IX_LOG_VERBOSE(@"User pressed 'Enter Password'");
-                                                break;
-                                                
-                                            default:
-                                                [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationUnconfigured];
-                                                IX_LOG_VERBOSE(@"TouchID not configured.");
-                                                break;
+                                    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                                        if (success) {
+                                            /* why is there a delay between when the user authenticates successfully
+                                             and the action execution?
+
+                                             - Can take as long as 20s before an Alert/Modify occurs visually.
+                                             - Appears to be a UI-only issue -- meaning that the action fires,
+                                             but the effect of the action is delayed visually.
+                                             - When it occurs, the UI is blocked and unresponsive
+                                             - Appears to only occur when the action affects the UI -- Log is immediate
+
+                                             */
+                                            IX_LOG_VERBOSE(@"User authenticated successfully.");
+                                            [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationSuccess];
+
+                                        } else {
+
+                                            switch (error.code) {
+                                                case LAErrorAuthenticationFailed:
+                                                    [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationFailed];
+                                                    IX_LOG_VERBOSE(@"Authenticated failed.");
+                                                    break;
+
+                                                case LAErrorUserCancel:
+                                                    [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationCancelled];
+                                                    IX_LOG_VERBOSE(@"User cancelled authentication.");
+                                                    break;
+
+                                                case LAErrorUserFallback:
+                                                    [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationPassword];
+                                                    IX_LOG_VERBOSE(@"User pressed 'Enter Password'");
+                                                    break;
+
+                                                default:
+                                                    [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationUnconfigured];
+                                                    IX_LOG_VERBOSE(@"TouchID not configured.");
+                                                    break;
+                                            }
                                         }
-                                    }
+
+                                    }];
                                 }];
         } else {
             [[self actionContainer] executeActionsForEventNamed:kIXAuthenticationUnavailable];
