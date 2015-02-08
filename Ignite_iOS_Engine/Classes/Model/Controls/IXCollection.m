a46f7a4f4004f7a78290d2c4bd8975987cbaed10
brandon <brandon@behemoth.ca>
2015-02-07 16:26:43 -0800

    IXCollection renamed

--- Ignite_iOS_Engine/Classes/Model/Controls/IXCollection.m
+++ Ignite_iOS_Engine/Classes/Model/Controls/IXCollection.m
@@ -26,22 +26,24 @@
 
 // IXCollection Attributes (Note: See IXCellBasedControl for the super classes properties as well.)
 IX_STATIC_CONST_STRING kIXMinimumLineSpacing = @"lineSpacing.min";
-IX_STATIC_CONST_STRING kIXMinimumInteritemSpacing = @"minimum_interitem_spacing";
-IX_STATIC_CONST_STRING kIXBackgroundSwipeWidth = @"background_swipe_width";
+IX_STATIC_CONST_STRING kIXMinimumInteritemSpacing = @"itemSpacing.min";
+IX_STATIC_CONST_STRING kIXBackgroundSwipeWidth = @"swipe.w";
+IX_STATIC_CONST_STRING kIXLayoutFlow = @"layoutFlow";
 
-IX_STATIC_CONST_STRING kIXLayoutFlow = @"layout_flow";
-IX_STATIC_CONST_STRING kIXLayoutFlowVertical = @"vertical";
-IX_STATIC_CONST_STRING kIXLayoutFlowHorizontal = @"horizontal";
+// Attribute Accepted Values
+IX_STATIC_CONST_STRING kIXLayoutFlowVertical = @"vertical"; // layoutFlow
+IX_STATIC_CONST_STRING kIXLayoutFlowHorizontal = @"horizontal"; // layoutFlow
 
 // IXCollection Events
-IX_STATIC_CONST_STRING kIXStartedScrolling = @"started_scrolling";
-IX_STATIC_CONST_STRING kIXEndedScrolling = @"ended_scrolling";
+IX_STATIC_CONST_STRING kIXStartedScrolling = @"didBeginScrolling";
+IX_STATIC_CONST_STRING kIXEndedScrolling = @"didEndScrolling";
 
 // IXCollection Functions
-IX_STATIC_CONST_STRING kIXResetAllBackgroundControls = @"reset_all_background_controls";
-IX_STATIC_CONST_STRING kIXSetBackgroundSwipeWidth = @"set_background_swipe_width";
+IX_STATIC_CONST_STRING kIXResetAllBackgroundControls = @"resetBgControls";
 
-IX_STATIC_CONST_STRING kIXCellIdentifier = @"IXUICollectionViewCell";
+#warning Why is this required?
+IX_STATIC_CONST_STRING kIXSetBackgroundSwipeWidth = @"setSwipeWidth";
+
 
 @interface IXCollection () <UICollectionViewDelegateFlowLayout,UICollectionViewDataSource>
 
@@ -50,126 +52,10 @@ IX_STATIC_CONST_STRING kIXCellIdentifier = @"IXUICollectionViewCell";
 
 @end
 
+// Internal properties
+IX_STATIC_CONST_STRING kIXCellIdentifier = @"IXUICollectionViewCell";
+
 @implementation IXCollection
-
-/*
-* Docs
-*
-*/
-
-/***************************************************************/
-
-/** This control has the following attributes:
-
-    @param minimum_line_spacing ?<br>*(string)*
-    @param minimum_interitem_spacing ?<br>*(string)*
-    @param background_swipe_width Background swipe width<br>*(string)*
-    @param layout_flow Layout Flow<br>*horizontal,vertical*
-    @param animate_reload Reload rows animation *(default: FALSE)*<br>*(bool)*
-    @param animate_reload.duration Reload rows animation duration<br>*(float)*
-    @param background.color <br>*(color)*
-    @param background_swipe_width <br>*(integer)*
-    @param background_slides_in_from_side <br>*(bool)*
-    @param background_swipe_adjusts_background_alpha <br>*(float)*
-    @param background_controls <br>*[array]*
-    @param section_header_xpath <br>*(string)*
-    @param section_header_controls <br>*[array]*
-    @param section_header_height <br>*(integer)*
-    @param section_header_width <br>*(integer)*
-    @param dataprovider_id <br>*(string)*
-    @param item_width <br>*(color)*
-    @param item_height <br>*(integer)*
-    @param paging.enabled <br>*(bool)*
-    @param scrollable <br>*(bool)*
-    @param pull_to_refresh.enabled <br>*(bool)*
-    @param pull_to_refresh.text <br>*(string)*
-    @param pull_to_refresh.text.color <br>*(color)*
-    @param pull_to_refresh.text.font <br>*()*
-    @param pull_to_refresh.tint.color <br>*(color)*
-"    @param scroll_indicator_style <br>*default
-black
-white**"
-    @param shows_scroll_indicators <br>*(bool)*
-    @param datarow.basepath <br>*(string)*
-
-*/
-
--(void)Attributes
-{
-}
-/***************************************************************/
-/***************************************************************/
-
-/** This control has the following attributes:
-*/
-
--(void)Returns
-{
-}
-
-/***************************************************************/
-/***************************************************************/
-
-/** This control fires the following events:
-
-
-    @param started_scrolling The ‘cancel’ button was pressed.
-    @param ended_scrolling The ‘%@’ button was pressed.
-
-*/
-
--(void)Events
-{
-}
-
-/***************************************************************/
-/***************************************************************/
-
-/** This control supports the following functions:
-
-
-    @param reset_all_background_controls Reset all background controls
-
- <pre class="brush: js; toolbar: false;">
- 
- </pre>
-
-
-    @param set_background_swipe_width Change the swipe width of the background controls
-
- <pre class="brush: js; toolbar: false;">
- 
- </pre>
-
-
-*/
-
--(void)Functions
-{
-}
-
-/***************************************************************/
-/***************************************************************/
-
-/** Go on, try it out!
-
- 
- <pre class="brush: js; toolbar: false;">
- 
- </pre>
-
-*/
-
--(void)Example
-{
-}
-
-/***************************************************************/
-
-/*
-* /Docs
-*
-*/
 
 -(void)dealloc
 {
