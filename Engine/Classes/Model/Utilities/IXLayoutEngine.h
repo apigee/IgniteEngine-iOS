cff3f1f48d52b0c586992929bfb8afb377eb62d1
brandon <brandon@behemoth.ca>
2015-04-24 13:42:57 -0700

    Added IXPicker from b917ac9351687b2317098e6eb1b4f70ca19017ec

---
+++ Engine/Classes/Model/Utilities/IXLayoutEngine.h
@@ -0,0 +1,21 @@
+//
+//  IXLayoutEngine.h
+//  Ignite iOS Engine (IX)
+//
+//  Created by Robert Walsh on 10/21/13.
+//  Copyright (c) 2013 Apigee, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class IXBaseControl;
+@class IXLayout;
+
+@interface IXLayoutEngine : NSObject
+
++(void)layoutControl:(IXLayout*)layoutControl inRect:(CGRect)layoutRect;
++(CGSize)getControlSize:(IXBaseControl*)control forLayoutSize:(CGSize)layoutSize;
++(CGSize)getPreferredSizeForLayoutControl:(IXLayout*)layoutControl forSuggestedSize:(CGSize)suggestedSize;
++(CGRect)getInternalLayoutRectForControl:(IXBaseControl*)control forOuterLayoutRect:(CGRect)outerLayoutRect;
+
+@end
