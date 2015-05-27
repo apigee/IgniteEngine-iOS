apigee/IgniteEngine-iOS

commit 2eecaac8f8f3977d836b70a10fe263e3b048ec28

Re-implemented previous changes retaining rowDataResultsDict stability

Classes/Model/DataProviders/IXHTTPDataProvider.m
@@ -151,7 +151,6 @@ static NSCache* sIXDataProviderCache = nil;
 @property (nonatomic,strong) NSString* acceptedContentType;
 @property (nonatomic,strong) NSString* requestType;
 @property (nonatomic,strong) NSString* responseType;
-//@property (nonatomic,copy) NSString* cacheID;
 @property (nonatomic,strong) NSMutableSet* acceptedContentTypes;
 @property (nonatomic,strong) NSDictionary* attachments;
 @property (nonatomic) NSURLRequestCachePolicy cachePolicy;
@@ -437,7 +436,6 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
         if (error)
         {
             [_response setResponseObject:error.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey]];
-            [_response setResponseStringFromObject:error.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey]];
             [_response setErrorMessage:error.localizedDescription];
             _paginationNextValue = nil;
             _paginationPrevValue = nil;
@@ -452,7 +450,6 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
                 }
             }
             [_response setResponseObject:responseObject];
-            [_response setResponseStringFromObject:responseObject];
             [self updatePaginationProperties];
         }
         
@@ -628,7 +625,7 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
     else if ([propertyName hasPrefix:kIXResponseBodyPrefix]) {
         NSString* prefix = [NSString stringWithFormat:@"%@%@", kIXResponseBodyPrefix, kIX_PERIOD_SEPARATOR];
         propertyName = [propertyName stringByReplacingOccurrencesOfString:prefix withString:kIX_EMPTY_STRING];
-        returnValue = [self stringForPath:propertyName container:_response.responseObject];
+        returnValue = [IXJSONUtils stringForPath:propertyName container:_response.responseObject sandox:self.sandbox baseObject:self];
     }
     else
     {
@@ -769,7 +766,6 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
     }
     if (wasModified) {
         [_response setResponseObject:modifiedResponseObject];
-        [_response setResponseStringFromObject:modifiedResponseObject];
         
         
         [self updateDataRowData];
@@ -782,6 +778,7 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
     }
 
 // TODO: re-implement predicate filtering:
+// Recommend we perform predicate filtering on the retrieval of data, not constantly update the rowDataResultsDict saved copy.
     
 //        NSString* modifyResponseType = [parameterContainer getStringPropertyValue:kIXModifyType defaultValue:nil];
 //        if( [modifyResponseType length] > 0 )
@@ -820,13 +817,12 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
     NSString* dataRowBasePath = [self dataRowBasePath];
     [self calculateAndStoreDataRowResultsForDataRowPath:dataRowBasePath];
     
-    for( NSString* dataRowKey in [[self rowDataResultsDict] allKeys] )
-    {
+    [[[self rowDataResultsDict] allKeys] enumerateObjectsUsingBlock:^(NSString* dataRowKey, NSUInteger idx, BOOL *stop) {
         if( ![dataRowKey isEqualToString:dataRowBasePath] )
         {
             [self calculateAndStoreDataRowResultsForDataRowPath:dataRowKey];
         }
-    }
+    }];
 }
 
 - (NSURLRequestCachePolicy)cachePolicyFromString:(NSString*)policy {
@@ -870,57 +866,40 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
         jsonObject = [IXJSONUtils objectForPath:dataRowPath container:_response.responseObject sandox:self.sandbox baseObject:self];
     }
     
-    NSArray* rowDataResults = nil;
+    NSMutableArray* rowDataResults = nil;
     if( [jsonObject isKindOfClass:[NSArray class]] )
     {
-        rowDataResults = (NSArray*)jsonObject;
+        rowDataResults = [(NSMutableArray*)jsonObject mutableCopy];
+        [[self rowDataResultsDict] setObject:rowDataResults forKey:(dataRowPath.length > 0) ? dataRowPath : kIXRowDataEmptyBasepathKey];
     }
-    if( rowDataResults )
+    
+// TODO: re-implement predicate filtering
+// Recommend we perform predicate filtering on the retrieval of data, not constantly update the rowDataResultsDict saved copy.
+    
+    /* predicate filtering has been disabled as of AFNetworking 2.0 implementation
+    if( rowDataResults.count > 0 && self.predicate != nil )
     {
         NSPredicate* predicate = [self predicate];
         if( predicate )
         {
-            rowDataResults = [rowDataResults filteredArrayUsingPredicate:predicate];
+            rowDataResults = [[rowDataResultsArray filteredArrayUsingPredicate:predicate] mutableCopy];
         }
         NSSortDescriptor* sortDescriptor = [self sortDescriptor];
         if( sortDescriptor )
         {
-            rowDataResults = [rowDataResults sortedArrayUsingDescriptors:@[sortDescriptor]];
+            rowDataResults = [[rowDataResultsArray sortedArrayUsingDescriptors:@[sortDescriptor]] mutableCopy];
         }
-    }
-    
-    if( [dataRowPath length] && rowDataResults != nil )
-    {
-        [[self rowDataResultsDict] setObject:rowDataResults forKey:dataRowPath];
-    }
-}
-
--(NSString*)rowDataRawStringResponse
-{
-    NSString* returnValue = nil;
-    NSArray* results = [self rowDataResultsDict][[self dataRowBasePath]];
-    if( [results count] > 0 )
-    {
-        NSError *error;
-        NSData* jsonData = [NSJSONSerialization dataWithJSONObject:results
-                                                           options:0
-                                                             error:&error];
-        if( [jsonData length] > 0 && error == nil )
-        {
-            NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-            returnValue = jsonString;
-        }
-    }
-    return returnValue;
+    }*/
 }
 
 -(NSString*)rowDataForIndexPath:(NSIndexPath*)rowIndexPath keyPath:(NSString*)keyPath dataRowBasePath:(NSString*)dataRowBasePath
 {
-    if( [dataRowBasePath length] <= 0 )
+    if( dataRowBasePath.length <= 0 && self.dataRowBasePath.length <= 0)
     {
-        dataRowBasePath = [self dataRowBasePath];
+        dataRowBasePath = kIXRowDataEmptyBasepathKey;
+    } else {
+        dataRowBasePath = self.dataRowBasePath;
     }
-
     NSString* returnValue = [super rowDataForIndexPath:rowIndexPath keyPath:keyPath dataRowBasePath:dataRowBasePath];
     if( keyPath && rowIndexPath )
     {
@@ -929,7 +908,7 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
         if( dataRowContainer != nil )
         {
             NSString* jsonKeyPath = [NSString stringWithFormat:@"%li.%@",(long)rowIndexPath.row,keyPath];
-            returnValue = [self stringForPath:jsonKeyPath container:dataRowContainer];
+            returnValue = [IXJSONUtils stringForPath:jsonKeyPath container:dataRowContainer sandox:self.sandbox baseObject:self];
         }
     }
     return returnValue;
@@ -937,48 +916,18 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
 
 -(NSUInteger)rowCount:(NSString *)dataRowBasePath
 {
-    if( [dataRowBasePath length] <= 0 )
+    if( dataRowBasePath.length <= 0 && self.dataRowBasePath.length <= 0)
     {
-        dataRowBasePath = [self dataRowBasePath];
+        dataRowBasePath = kIXRowDataEmptyBasepathKey;
+    } else {
+        dataRowBasePath = self.dataRowBasePath;
     }
-
     if( [self rowDataResultsDict][dataRowBasePath] == nil )
     {
         [self calculateAndStoreDataRowResultsForDataRowPath:dataRowBasePath];
     }
 
     return [[self rowDataResultsDict][dataRowBasePath] count];
-}
-
--(NSString*)stringForPath:(NSString*)jsonXPath container:(NSObject*)container
-{
-    NSString* returnValue = nil;
-    NSObject* jsonObject = [IXJSONUtils objectForPath:jsonXPath container:container sandox:self.sandbox baseObject:self];
-    if( jsonObject )
-    {
-        if( [jsonObject isKindOfClass:[NSString class]] ) {
-            returnValue = (NSString*)jsonObject;
-        } else if( [jsonObject isKindOfClass:[NSNumber class]] ) {
-            returnValue = [((NSNumber*)jsonObject) stringValue];
-        } else if( [jsonObject isKindOfClass:[NSNull class]] ) {
-            returnValue = nil;
-        } else if( [NSJSONSerialization isValidJSONObject:jsonObject] ) {
-            
-            NSError* __autoreleasing jsonConvertError = nil;
-            NSData* jsonData = [NSJSONSerialization dataWithJSONObject:jsonObject
-                                                               options:NSJSONWritingPrettyPrinted
-                                                                 error:&jsonConvertError];
-            
-            if( jsonConvertError == nil && jsonData ) {
-                returnValue = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-            } else {
-                IX_LOG_WARN(@"WARNING from %@ in %@: Error Converting JSON object: %@",THIS_FILE,THIS_METHOD,[jsonConvertError description]);
-            }
-        } else {
-            IX_LOG_WARN(@"WARNING from %@ in %@: Invalid JSON Object: %@",THIS_FILE,THIS_METHOD,[jsonObject description]);
-        }
-    }
-    return returnValue;
 }
 
 @end

Classes/Model/Controls/IXTable.m
@@ -59,7 +59,7 @@ IX_STATIC_CONST_STRING kIXSEPARATORStyleDefault = @"default";
 IX_STATIC_CONST_STRING kIXStartedScrolling = @"didBeginScrolling";
 IX_STATIC_CONST_STRING kIXEndedScrolling = @"didEndScrolling";
 
-// These Events fired on the actual cells. (aka dataRow will work)
+// These Events fired on the actual cells - calling a $row eval will work
 IX_STATIC_CONST_STRING kIXWillDisplayCell = @"willDisplayCell";
 IX_STATIC_CONST_STRING kIXDidHideCell = @"didHideCell";
 IX_STATIC_CONST_STRING kIXDidSelectCell = @"didSelectCell";

Classes/Model/DataProviders/IXBaseDataProvider.h
@@ -47,12 +47,7 @@ extern NSString* IXBaseDataProviderDidUpdateNotification;
 @property (nonatomic,strong) NSString* method;
 @property (nonatomic,strong) NSDictionary* body;
 @property (nonatomic,strong) NSDictionary* queryParams;
-//@property (nonatomic,copy,readonly) NSString* fullDataLocation;
 @property (nonatomic,strong) NSString* url;
-//@property (nonatomic,copy,readonly) NSString* dataPath;
-
-//-(void)createRequest;
-//-(NSURLRequest*)createURLRequest;
 
 -(void)loadData:(BOOL)forceGet;
 -(void)loadData:(BOOL)forceGet paginationKey:(NSString*)paginationKey;

Classes/Model/DataProviders/IXDataRowDataProvider.h
@@ -27,6 +27,9 @@
 
 #import "IXBaseDataProvider.h"
 
+// Internal Properties
+IX_STATIC_CONST_STRING kIXRowDataEmptyBasepathKey = @"kIXDataRowEmptyBasepathKey";
+
 @interface IXDataRowDataProvider : IXBaseDataProvider
 
 @property (nonatomic,copy,readonly) NSString* dataRowBasePath;
@@ -39,7 +42,6 @@
 @property (nonatomic,readonly) NSSortDescriptor* sortDescriptor;
 
 -(NSUInteger)rowCount:(NSString*)dataRowBasePath;
--(NSString*)rowDataRawStringResponse;
 -(NSString*)rowDataForIndexPath:(NSIndexPath*)rowIndexPath keyPath:(NSString*)keyPath dataRowBasePath:(NSString*)dataRowBasePath;
 -(NSString*)rowDataTotalForKeyPath:(NSString*)keyPath;
 

Classes/Model/DataProviders/IXDataRowDataProvider.m
@@ -168,7 +168,7 @@ IX_STATIC_CONST_STRING kIXTotal = @"total.";
     NSDecimalNumber* rowTotal = [NSDecimalNumber zero];
     for( int i = 0; i < rowCount; i++ )
     {
-        NSString* rowDataForIndex = [self rowDataForIndexPath:[NSIndexPath indexPathForRow:i inSection:0] keyPath:keyPath dataRowBasePath:[self dataRowBasePath]];
+        NSString* rowDataForIndex = [self rowDataForIndexPath:[NSIndexPath indexPathForRow:i inSection:0] keyPath:keyPath dataRowBasePath:self.dataRowBasePath];
         if( rowDataForIndex )
         {
             NSDecimalNumber* decimalNumber = [NSDecimalNumber decimalNumberWithString:rowDataForIndex];

Classes/Model/DataProviders/IXHTTPDataProvider.h
@@ -33,7 +33,6 @@ typedef void(^LoadFinished)(BOOL success, NSURLSessionDataTask* task, id respons
 // Internal properties
 IX_STATIC_CONST_STRING kIXProgressKVOKey = @"fractionCompleted";
 
-
 @interface IXHTTPDataProvider : IXDataRowDataProvider
 
 @property (nonatomic,strong) NSMutableDictionary* rowDataResultsDict;

Classes/Model/DataProviders/IXSocketDataProvider.m
@@ -128,7 +128,6 @@ IX_STATIC_CONST_STRING kIXDataDictionaryKey = @"data";
         [_messageDictionary setObject:messageArray forKey:kIXDataDictionaryKey];
         
         [self.response setResponseObject:_messageDictionary];
-        [self.response setResponseStringFromObject:_messageDictionary];
         [[self actionContainer] executeActionsForEventNamed:KIXMessageReceived];
     }
 }

Classes/Model/Utilities/IXHTTPResponse.h
@@ -41,6 +41,4 @@
 @property (nonatomic) CFAbsoluteTime requestStartTime; // = CFAbsoluteTimeGetCurrent();
 @property (nonatomic) CFAbsoluteTime requestEndTime;
 
--(void)setResponseStringFromObject:(NSObject*)object;
-
 @end

commit 87db1b27fcccc3d4d8704190fc7659edc79af315

Fixed dataRow-related bug introduced in 2eecaac8f8f3977d836b70a10fe263e3b048ec28

Classes/Model/DataProviders/IXHTTPDataProvider.m
@@ -894,11 +894,7 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
 
 -(NSString*)rowDataForIndexPath:(NSIndexPath*)rowIndexPath keyPath:(NSString*)keyPath dataRowBasePath:(NSString*)dataRowBasePath
 {
-    if( dataRowBasePath.length <= 0 && self.dataRowBasePath.length <= 0) {
-        dataRowBasePath = kIXRowDataEmptyBasepathKey;
-    } else {
-        dataRowBasePath = self.dataRowBasePath;
-    }
+    dataRowBasePath = [self determineCorrectDataRowBasePathForInstance:dataRowBasePath];
     
     NSString* returnValue = [super rowDataForIndexPath:rowIndexPath keyPath:keyPath dataRowBasePath:dataRowBasePath];
     if( keyPath && rowIndexPath )
@@ -916,18 +912,26 @@ IX_STATIC_CONST_STRING kIXLocationSuffixCache = @".cache";
 
 -(NSUInteger)rowCount:(NSString *)dataRowBasePath
 {
-    if( dataRowBasePath.length <= 0 && self.dataRowBasePath.length <= 0)
-    {
-        dataRowBasePath = kIXRowDataEmptyBasepathKey;
-    } else {
-        dataRowBasePath = self.dataRowBasePath;
-    }
+    dataRowBasePath = [self determineCorrectDataRowBasePathForInstance:dataRowBasePath];
+    
     if( [self rowDataResultsDict][dataRowBasePath] == nil )
     {
         [self calculateAndStoreDataRowResultsForDataRowPath:dataRowBasePath];
     }
 
     return [[self rowDataResultsDict][dataRowBasePath] count];
+}
+
+-(NSString*)determineCorrectDataRowBasePathForInstance:(NSString*)dataRowBasePath {
+    if (dataRowBasePath.length <= 0 && self.dataRowBasePath.length <= 0)
+    {
+        dataRowBasePath = kIXRowDataEmptyBasepathKey;
+    }
+    else if (dataRowBasePath.length <= 0)
+    {
+        dataRowBasePath = self.dataRowBasePath;
+    }
+    return dataRowBasePath;
 }
 
 @end